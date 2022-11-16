9cfc59d85ae935c28826b7c3997affc3940e6669
Catalina Sorina Gherzan <catalina.sorina@catalina.local>
2022-11-16 18:11:06 +0200

    wip

--- ibm-dev-dealer-fe/components/Layout/Layout.jsx
+++ ibm-dev-dealer-fe/components/Layout/Layout.jsx
@@ -26,6 +26,9 @@ const Layout = ({ title = 'IBM Dev Dealer', logged = false, isPM, children }) =>
           {logged && <Link href={profile}>Profile</Link>}
           {isPM && <Link href={manageEmployees}>Manage Employees</Link>}
         </nav>
+        <div className={`pointer-events-auto ml-8 rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500 ${styles.test}`}>
+          Tailwind Salut Ceva text
+        </div>
         <div>{children}</div>
         <footer>Copy rights AMAZING TEAM DEV DEALER</footer>
       </div>
