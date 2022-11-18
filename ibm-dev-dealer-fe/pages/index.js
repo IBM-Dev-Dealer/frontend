66a39662ba43e2284e7f86b57d9ff72886e9b082
Catalina Sorina Gherzan <catalina.sorina@catalina.local>
2022-11-18 23:15:36 +0200

    added date picker

--- ibm-dev-dealer-fe/pages/index.js
+++ ibm-dev-dealer-fe/pages/index.js
@@ -1,7 +1,13 @@
 import Home from '../components/page-components/Home/Home';
+import PickDate from '../components/atoms/PickDate/PickDate';
 
 const HomePage = () => {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <PickDate />
+    </>
+  );
 };
 
 export default HomePage;
