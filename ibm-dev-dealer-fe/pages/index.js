d98cc58c0b8d8030a7cfc61407253584acd24d12
Catalina Sorina Gherzan <catalina.sorina@catalina.local>
2022-11-18 22:47:35 +0200

    added loader

--- ibm-dev-dealer-fe/pages/index.js
+++ ibm-dev-dealer-fe/pages/index.js
@@ -1,7 +1,13 @@
 import Home from '../components/page-components/Home/Home';
+import Loader from '../components/atoms/Loader/Loader';
 
 const HomePage = () => {
-  return <Home />;
+  return (
+    <div>
+      <Home />
+      <Loader />
+    </div>
+  );
 };
 
 export default HomePage;
