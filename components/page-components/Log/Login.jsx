1bc7dd285d1151ef911047946b2ab677eb7ab1f9
GeorgeBotezatu <59911746+GeorgeBotezatu@users.noreply.github.com>
2022-11-22 15:29:56 +0200

    created input structure

--- components/page-components/Log/Login.jsx
+++ components/page-components/Log/Login.jsx
@@ -1,14 +1,16 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { ROUTES } from '../../../utils/utils';
-import Title from '../../atoms/Title/Title';
+import { ROUTES } from "../../../utils/utils";
+import TextInput from "../../atoms/TextInput/TextInput";
+import Title from "../../atoms/Title/Title";
 
 const Login = () => {
-  return (
-    <Title>
-      Login or <Link href={ROUTES.REGISTER}>Register</Link>
-    </Title>
-  );
+	return (
+		<Title>
+			Login or <Link href={ROUTES.REGISTER}>Register</Link>
+			<TextInput />
+		</Title>
+	);
 };
 
 export default Login;
--- components/page-components/Log/Register.jsx
+++ components/page-components/Log/Register.jsx
@@ -1,14 +1,16 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { ROUTES } from '../../../utils/utils';
-import Title from '../../atoms/Title/Title';
+import { ROUTES } from "../../../utils/utils";
+import TextInput from "../../atoms/TextInput/TextInput";
+import Title from "../../atoms/Title/Title";
 
 const Register = () => {
-  return (
-    <Title>
-      Register or <Link href={ROUTES.LOG}>Login</Link>
-    </Title>
-  );
+	return (
+		<Title>
+			Register or <Link href={ROUTES.LOG}>Login</Link>
+			<TextInput />
+		</Title>
+	);
 };
 
 export default Register;
