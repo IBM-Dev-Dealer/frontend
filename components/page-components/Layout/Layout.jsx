d607a4ff304e94f508a847fafc940dc3c1227d1f
Cătălina Sorina <41907279+CatalinaSorina@users.noreply.github.com>
2022-11-22 12:06:08 +0200

    update Loader and added header (#8)
    Co-authored-by: Catalina Sorina Gherzan <catalina.sorina@catalina.local>

--- components/page-components/Layout/Layout.jsx
+++ components/page-components/Layout/Layout.jsx
@@ -1,10 +1,10 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-import { ROUTES, getPageBackground, getTitle } from '../../../utils/utils';
+import { ROUTES, getPageBackground, getTitle } from "../../../utils/utils";
 
-import styles from './Layout.module.scss';
+import styles from "./Layout.module.scss";
 
 const Layout = ({ logged = false, isPM = false, children }) => {
   const { pathname } = useRouter();
@@ -16,37 +16,44 @@ const Layout = ({ logged = false, isPM = false, children }) => {
       <Head>
         <title>{title}</title>
       </Head>
-      <div className={styles.logoWrapper}>logo</div>
       <div className={styles.layout}>
-        <nav className={styles.nav}>
-          <Link className={styles.background1} href={ROUTES.DEFAULT_PAGE}>
-            HOME
-          </Link>
-          <Link className={styles.background2} href={ROUTES.CURRENT_PROJECT}>
-            CURRENT PROJECT
-          </Link>
-          <Link className={styles.background3} href={ROUTES.OTHER_PROJECTS}>
-            OTHER PROJECTS
-          </Link>
-          <Link className={styles.background4} href={ROUTES.SOURCE}>
-            SOURCE
-          </Link>
-          {logged && (
-            <Link className={styles.background5} href={ROUTES.PROFILE}>
-              PROFILE
+        <header className={styles.header}>
+          <div className={styles.logoWrapper}>logo</div>
+          <nav className={styles.nav}>
+            <Link className={styles.background1} href={ROUTES.DEFAULT_PAGE}>
+              HOME
             </Link>
-          )}
-          {isPM && (
-            <Link className={styles.background6} href={ROUTES.MANAGE_EMPLOYEES}>
-              MANAGE EMPLOYEES
+            <Link className={styles.background2} href={ROUTES.CURRENT_PROJECT}>
+              CURRENT PROJECT
             </Link>
-          )}
-          <Link className={styles.background7} href={ROUTES.LOG}>
-            {logged ? 'LOGOUT' : 'LOGIN/REGISTER'}
-          </Link>
-        </nav>
+            <Link className={styles.background3} href={ROUTES.OTHER_PROJECTS}>
+              OTHER PROJECTS
+            </Link>
+            <Link className={styles.background4} href={ROUTES.SOURCE}>
+              SOURCE
+            </Link>
+            {logged && (
+              <Link className={styles.background5} href={ROUTES.PROFILE}>
+                PROFILE
+              </Link>
+            )}
+            {isPM && (
+              <Link
+                className={styles.background6}
+                href={ROUTES.MANAGE_EMPLOYEES}
+              >
+                MANAGE EMPLOYEES
+              </Link>
+            )}
+            <Link className={styles.background7} href={ROUTES.LOG}>
+              {logged ? "LOGOUT" : "LOGIN/REGISTER"}
+            </Link>
+          </nav>
+        </header>
 
-        <div className={`${styles.wrapper} ${styles[pageBackground]}`}>{children}</div>
+        <div className={`${styles.wrapper} ${styles[pageBackground]}`}>
+          {children}
+        </div>
 
         <footer className={`${styles.footer} ${styles[pageBackground]}`}>
           ©2022 DECÂT O ECHIPĂ
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import Home from '../components/page-components/Home/Home';
-import PickDate from '../components/atoms/PickDate/PickDate';
-import Loader from '../components/atoms/Loader/Loader';
 
 const HomePage = () => {
   return (
     <>
       <Home />
-      <PickDate />
-      <Loader />
     </>
   );
 };
