6dd18081e48706121adb1cfda0c712395dff67a6
GeorgeBotezatu <59911746+GeorgeBotezatu@users.noreply.github.com>
2022-11-28 12:23:35 +0200

    created login form

--- components/page-components/Log/Login.jsx
+++ components/page-components/Log/Login.jsx
@@ -1,60 +1,71 @@
 import TextInput from "../../atoms/TextInput/TextInput";
+import { linkColor } from "./Log.module.scss";
 import Title from "../../atoms/Title/Title";
 import { Form, Formik } from "formik";
 import * as yup from "yup";
 import Link from "next/link";
 import { ROUTES } from "../../../utils/utils";
-
+import Button from "../../atoms/Button/Button";
+import classNames from "classnames";
 const Login = () => {
-  const initialValues = {
-    email: "",
-    password: "",
-  };
+	const initialValues = {
+		email: "",
+		password: "",
+	};
 
-  const validate = yup.object({
-    email: yup.string().email("Invalid Email").required("Email field empty"),
-    password: yup.string().required("Password field empty"),
-  });
+	const validate = yup.object({
+		email: yup.string().required("Email field empty"),
+		password: yup.string().required("Password field empty"),
+	});
 
-  return (
-    <>
-      <Title>
-        Login or <Link href={ROUTES.REGISTER}>Register</Link>
-      </Title>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validate}
-        onSubmit={(values) => {
-          //   submitHandler(values);
-          console.log("values", values);
-        }}
-      >
-        {(formik) => (
-          <div>
-            <Form>
-              <TextInput
-                type='email'
-                id='email'
-                name='email'
-                placeholder='Email'
-                labelText='Email'
-              />
+	const loginFormTailwindContainer = "flex items-center flex-col w-full mt-24";
 
-              <TextInput
-                type='password'
-                id='password'
-                name='password'
-                placeholder='Password'
-                labelText='Password'
-              />
+	return (
+		<Formik
+			initialValues={initialValues}
+			validationSchema={validate}
+			onSubmit={(values) => {
+				//   submitHandler(values);
+				console.log("values", values);
+			}}
+		>
+			{(formik) => (
+				<div className={loginFormTailwindContainer}>
+					<Title>Welcome back!</Title>
 
-              <button type='submit'>Submit</button>
-            </Form>
-          </div>
-        )}
-      </Formik>
-    </>
-  );
+					<Form className="w-80 flex gap-2 flex-col">
+						<TextInput
+							type="email"
+							id="email"
+							name="email"
+							placeholder="Email"
+							labelText="Email"
+						/>
+
+						<TextInput
+							type="password"
+							id="password"
+							name="password"
+							placeholder="Password"
+							labelText="Password"
+						/>
+
+						<Button type="submit" label="Submit" />
+					</Form>
+
+					<p className="mt-2 text-xs">
+						Not registered yet?{" "}
+						<Link
+							className={classNames("text-xs", linkColor)}
+							href={ROUTES.REGISTER}
+						>
+							Clik Here!
+						</Link>
+					</p>
+				</div>
+			)}
+		</Formik>
+	);
 };
 
 export default Login;
