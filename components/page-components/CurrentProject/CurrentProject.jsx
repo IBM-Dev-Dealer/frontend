0e872c063ae2083d4deffd530299f0e255bee1fe
Catalina Sorina Gherzan <catalina.sorina@catalina.local>
2022-11-29 15:17:56 +0200

    added Avatar and updated PickDate and Client components

--- components/page-components/CurrentProject/CurrentProject.jsx
+++ components/page-components/CurrentProject/CurrentProject.jsx
@@ -1,10 +1,8 @@
-import Title from "../../atoms/Title/Title";
 import Client from "./Client/Client";
 
 const CurrentProject = ({ client }) => {
   return (
     <div>
-      <Title>Current Project</Title>
       <Client {...client} />
     </div>
   );
