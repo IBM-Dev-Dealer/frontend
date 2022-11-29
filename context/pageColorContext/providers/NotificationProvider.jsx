4947c36091f12713881667cc9fef00244ed7f932
Dragos-Bocancea <dragos.bocancea@ibm.com>
2022-11-29 16:53:41 +0200

    delete dummy notifications

--- context/pageColorContext/providers/NotificationProvider.jsx
+++ context/pageColorContext/providers/NotificationProvider.jsx
@@ -3,18 +3,7 @@ import { createContext, useCallback, useState } from "react";
 export const NotificationContext = createContext();
 
 export const NotificationContextProvider = ({ children }) => {
-  const [notifications, setNotifications] = useState([
-    {
-      kind: "success",
-      message: "This is a test messae erthe rt hfrjythertnert hw etg sr th bwet g a4 a5h srt hsr5.",
-      id: "id1",
-    },
-    {
-      kind: "error",
-      message: "Thidsadasdsdasw etg sr th bwet g a4 a5h srt hsr5.",
-      id: "id2",
-    },
-  ]);
+  const [notifications, setNotifications] = useState([]);
 
   const notify = useCallback(
     (newNotification) =>
