82bb121a66dd682de0e2e547d6f2a92542e2c1d7
Dragos-Bocancea <dragos.bocancea@ibm.com>
2022-11-29 15:47:37 +0200

    .

--- pages/api/getFields.js
+++ pages/api/getFields.js
@@ -1,20 +1,20 @@
-import { ALL_FIELDS } from "./mock/addProjectMock";
+// const getFields = async (req, res) => {
+//   if (req.method === "GET" && req.headers.authorization === "authorization") {
+//     const queryParamKeys = Object.keys(req.query);
 
-const getFields = async (req, res) => {
-  if (req.method === "GET" && req.headers.authorization === "authorization") {
-    const queryParamKeys = Object.keys(req.query);
+//     if (queryParamKeys.includes("fields")) {
+//       const fields = {};
 
-    if (queryParamKeys.includes("fields")) {
-      const fields = {};
+//       req.query.fields.forEach((paramKey) => {
+//         const foundField = ALL_FIELDS.find((field) => field.codename === paramKey);
+//         if (foundField) fields[foundField.codename] = foundField;
+//       });
+//       return res.status(200).json({ fields });
+//     }
+//   }
 
-      req.query.fields.forEach((paramKey) => {
-        const foundField = ALL_FIELDS.find((field) => field.codename === paramKey);
-        if (foundField) fields[foundField.codename] = foundField;
-      });
-      return res.status(200).json({ fields });
-    }
-  }
+//   return res.status(500).json({ error: "Please authenticate!", logged: false });
+// };
 
-  return res.status(500).json({ error: "Please authenticate!", logged: false });
-};
+const getFields = async (queryParams) => ({ fields });
 export default getFields;
