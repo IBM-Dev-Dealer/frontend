004f8936f38765ddca871a29d9f05a3e88a01780
GeorgeBotezatu <59911746+GeorgeBotezatu@users.noreply.github.com>
2022-11-30 16:01:01 +0200

    partially done the form (#30)
    * partially done the form
    
    * finished form
    
    * Deleted comments

--- components/page-components/Log/Login.jsx
+++ components/page-components/Log/Login.jsx
@@ -8,55 +8,63 @@ import { ROUTES } from "../../../utils/utils";
 import Button from "../../atoms/Button/Button";
 import classNames from "classnames";
 const Login = () => {
-  const initialValues = {
-    email: "",
-    password: "",
-  };
+	const initialValues = {
+		email: "",
+		password: "",
+	};
 
-  const validate = yup.object({
-    email: yup.string().required("Email field empty"),
-    password: yup.string().required("Password field empty"),
-  });
+	const validate = yup.object({
+		email: yup.string().required("Email field empty"),
+		password: yup.string().required("Password field empty"),
+	});
 
-  const loginFormTailwindContainer = "flex items-center flex-col w-full mt-24";
+	const loginFormTailwindContainer = "flex items-center flex-col w-full mt-24";
+	const formTailwind = "w-80 flex gap-2 flex-col";
+	return (
+		<Formik
+			initialValues={initialValues}
+			validationSchema={validate}
+			onSubmit={(values) => {
+				//   submitHandler(values);
+				console.log("values", values);
+			}}
+		>
+			{(formik) => (
+				<div className={loginFormTailwindContainer}>
+					<Title>Welcome back!</Title>
+					<Form className={formTailwind}>
+						<TextInput
+							type="email"
+							id="email"
+							name="email"
+							placeholder="Email"
+							labelText="Email"
+						/>
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validate}
-      onSubmit={(values) => {
-        //   submitHandler(values);
-        console.log("values", values);
-      }}
-    >
-      {(formik) => (
-        <div className={loginFormTailwindContainer}>
-          <Title>Welcome back!</Title>
+						<TextInput
+							type="password"
+							id="password"
+							name="password"
+							placeholder="Password"
+							labelText="Password"
+						/>
 
-          <Form className='w-80 flex gap-2 flex-col'>
-            <TextInput type='email' id='email' name='email' placeholder='Email' labelText='Email' />
+						<Button type="submit" label="Submit" />
+					</Form>
 
-            <TextInput
-              type='password'
-              id='password'
-              name='password'
-              placeholder='Password'
-              labelText='Password'
-            />
-
-            <Button type='submit' label='Submit' />
-          </Form>
-
-          <p className='mt-2 text-xs'>
-            Not registered yet?{" "}
-            <Link className={classNames("text-xs", linkColor)} href={ROUTES.REGISTER}>
-              Click Here!
-            </Link>
-          </p>
-        </div>
-      )}
-    </Formik>
-  );
+					<p className="mt-2 text-xs">
+						Not registered yet?{" "}
+						<Link
+							className={classNames("text-xs", linkColor)}
+							href={ROUTES.REGISTER}
+						>
+							Click Here!
+						</Link>
+					</p>
+				</div>
+			)}
+		</Formik>
+	);
 };
 
 export default Login;
