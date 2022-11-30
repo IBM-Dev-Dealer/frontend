b794bb3ce41504896c4db19d93f67a348f74d564
Dragos-Bocancea <dragos.bocancea@gmail.com>
2022-11-30 18:13:04 +0200

    wrap up

--- components/molecules/ProjectsTable/ProjectsTable.jsx
+++ components/molecules/ProjectsTable/ProjectsTable.jsx
@@ -1,4 +1,6 @@
+import { colorizeJSXArray } from "../../../utils/utils";
 import ColoredItems from "../../atoms/ColoredItems/ColoredItems";
+import { PAGE_DESCRIPTION } from "./constants";
 
 const formatData = (type, data) => {
   switch (type) {
@@ -6,21 +8,36 @@ const formatData = (type, data) => {
       return (
         <div className='flex flex-col gap-2'>
           <div className='flex flex-col'>
-            <span className='text-sm text-green font-bold'>Start:</span>{" "}
+            <span className='text-sm text-green font-bold'>Start:</span>
             <span className='text-md'>{data.start}</span>
           </div>
           <div className='flex flex-col'>
-            <span className='text-sm text-green font-bold'>End:</span>{" "}
+            <span className='text-sm text-green font-bold'>End:</span>
             <span className='text-md'>{data.end}</span>
           </div>
         </div>
       );
     case "technologies":
       return (
-        <div className='w-100'>
-          <div className='flex flex-col gap-2'>
-            <ColoredItems items={data.map((d) => d.label)} canDelete={false} />
-          </div>
+        <div className='flex flex-col gap-2'>
+          <ColoredItems items={data.map((d) => d.label)} canDelete={false} />
+        </div>
+      );
+    case "requiredCapacity":
+      return (
+        <div className='flex flex-col gap-2'>
+          {colorizeJSXArray(
+            data.map((d, i) => (
+              <div key={i} className='flex flex-col rounded-md shadow-md p-2 gap-1'>
+                {/* <span className='text-xs font-bold'>Developers:</span> */}
+                <span className='text-sm self-end'>{d.developers}</span>
+                {/* <span className='text-xs font-bold'>Seniority:</span> */}
+                <span className='text-sm self-end'>{d.seniority.label}</span>
+                {/* <span className='text-xs font-bold'>Technology:</span> */}
+                <span className='text-sm self-end'>{d.technology.label}</span>
+              </div>
+            )),
+          )}
         </div>
       );
     default:
@@ -34,38 +51,35 @@ const ProjectsTable = ({ projects, fields: tableFields }) => {
   console.log("projects", projects);
 
   return (
-    <table
-      align='center'
-      className='rounded-2xl border shadow-md overflow-auto flex flex-col w-100 border-collapse table-fixed'
-    >
-      <tbody>
-        <tr key='projectstable-header'>
-          {fields.map((field) => (
-            <td
-              key={field}
-              id='col'
-              className='hover:bg-tdansparent-gray-05 bg-light-green px-4 py-1 font-bold'
-            >
-              {tableFields[field].label}
-            </td>
-          ))}
-        </tr>
-
-        {projects.map((project) => (
-          <tr
-            key={project.projectId}
-            className='hover:bg-transparent-gray-05 cursor-pointer border-b'
-          >
+    <>
+      <div className='mb-12 text-md text-transparent-gray-50 max-w-2xl text-justify m-auto'>
+        {PAGE_DESCRIPTION}
+      </div>
+      <table align='center' className='overflow-scroll border-collapse table-fixed'>
+        <tbody>
+          <tr key='projectstable-header'>
             {fields.map((field) => (
-              <td key={`${project.projectId}_${field}`} className='px-4 py-2'>
-                {/* {project[field].toString()} */}
-                {formatData(field, project[field])}
+              <td key={field} id='col' className='bg-light-green px-4 py-1 font-bold'>
+                {tableFields[field].label}
               </td>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+
+          {projects.map((project) => (
+            <tr
+              key={project.projectId}
+              className='hover:bg-transparent-gray-05 hover:shadow-md cursor-pointer border border-x-transparent'
+            >
+              {fields.map((field) => (
+                <td key={`${project.projectId}_${field}`} className='px-4 py-2'>
+                  {formatData(field, project[field])}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
