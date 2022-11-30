4258120376e406250f4770697fc6d1b70127a54e
Dragos-Bocancea <dragos.bocancea@gmail.com>
2022-11-30 16:10:04 +0200

    lint

--- pages/log.js
+++ pages/log.js
@@ -1,6 +1,6 @@
 import Login from "../components/page-components/Log/Login";
 const LogPage = ({ logged, logout = () => {} }) => {
-	return logged ? logout() : <Login />;
+  return logged ? logout() : <Login />;
 };
 
 export default LogPage;
