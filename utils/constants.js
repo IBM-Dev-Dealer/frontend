e04516b641f494d890d32c4b3cb7746c648dc5fc
GeorgeBotezatu <59911746+GeorgeBotezatu@users.noreply.github.com>
2022-12-01 12:35:06 +0200

    Added notifications (#36)
    * Added notifications
    
    * added notification on dev form
    
    * solved conflicts
    
    * Created constants file

---
+++ utils/constants.js
@@ -0,0 +1,21 @@
+//Notification types
+export const NOTIFICATION_SUCCESS = "success";
+export const NOTIFICATION_ERROR = "error";
+export const NOTIFICATION_INFO = "info";
+
+//notification IDs
+export const REGISTER_NOTIFICATION_SUCCESS_ID = "registerSucces";
+export const REGISTER_NOTIFICATION_ERROR_ID = "registerError";
+export const LOGIN_NOTIFICATION_SUCCESS_ID = "loginSucces";
+export const LOGIN_NOTIFICATION_ERROR_ID = "loginError";
+export const FEEDBACK_NOTIFICATION_SUCCESS_ID = "feedbackSucces";
+export const FEEDBACK_NOTIFICATION_ERROR_ID = "feedbackError";
+export const ADD_PROJECT_NOTIFICATION_SUCCESS_ID = "addProjectSucces";
+export const ADD_PROJECT_NOTIFICATION_ERROR_ID = "addProjectError";
+
+//notification messages
+export const SERVER_ERROR = "Server Error";
+export const REGISTER_SUCCESS = "Registered successfully!";
+export const FEEDBACK_SUCCESS = "Feedback sent successfully!";
+export const LOGIN_SUCCESS = "Logged in successfully!";
+export const ADD_PROJECT_SUCCESS = "Project added successfully!";
