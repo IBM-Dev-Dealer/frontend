b4c85df7ae423a0cad1f2ce14c353ef90353c352
Dragos-Bocancea <dragos.bocancea@gmail.com>
2022-12-04 18:06:35 +0200

    .

--- pages/feedback/give-feedback.js
+++ pages/feedback/give-feedback.js
@@ -13,7 +13,7 @@ export const getStaticProps = async () => {
   // const authorization = "authorization";
 
   const loggedUserRoles = ["project-manager", "dev"];
-  const projectIDs = [15];
+  const projectIDs = [2, 3];
 
   const getProjects = async () => {
     const projects = projectIDs.map((p) => callAPI(`/projects/${p}`).then((r) => r.json()));
